ae2d5361a3be10d9af9e75d193fe92495f227ed1
wesleykyle2005 <wesleykyleruizcenteno@gmail.com>
2025-03-21 04:38:09 -0600

    Añadir distintos estilos

--- Stores/static/stores/js/maps.js
+++ Stores/static/stores/js/maps.js
@@ -7,13 +7,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+// Definir un ícono personalizado
+var customIcon = L.icon({
+    iconUrl: '/static/stores/images/custom-marker.png', // Ruta al ícono personalizado
+    iconSize: [60, 60], // Tamaño del ícono
+    iconAnchor: [30, 60], // Punto del ícono que se posicionará en las coordenadas
+    popupAnchor: [0, -60] // Punto desde donde se abrirá el popup
+});
+
 // Añadir marcadores de tiendas
 var stores = JSON.parse(document.getElementById('store-data').textContent);
 console.log(stores);
 
 stores.forEach(function(store) {
-    var marker = L.marker([store.lat, store.lng]).addTo(map);
-    marker.bindPopup("<b>" + store.name + "</b><br>" + store.description + "<br><a href='" + store.url + "'>Ver productos</a>");
+    var marker = L.marker([store.lat, store.lng], { icon: customIcon }).addTo(map); // Usar el ícono personalizado
+    marker.bindPopup(
+        `<b>${store.name}</b><br>${store.description}<br><a href="${store.url}" class="btn btn-primary btn-sm mt-2">Ver productos</a>`
+    );
     console.log(store);
 });
 
