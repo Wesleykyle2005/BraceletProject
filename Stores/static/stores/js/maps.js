6496e0f0a0ffbd92ab0eadb9667fece861704a17
Wesleykyle2005 <123531906+Wesleykyle2005@users.noreply.github.com>
2025-03-21 06:51:45 -0600

    Update maps.js

--- Stores/static/stores/js/maps.js
+++ Stores/static/stores/js/maps.js
@@ -1,5 +1,5 @@
 // Inicializar el mapa
-var map = L.map('map').setView([0, 0], 2); // Coordenadas iniciales y zoom
+var map = L.map('map').setView([0, 0], 2);
 
 // Añadir capa de mapa base
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,10 +9,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Definir un ícono personalizado
 var customIcon = L.icon({
-    iconUrl: '/static/stores/images/custom-marker.png', // Ruta al ícono personalizado
-    iconSize: [60, 60], // Tamaño del ícono
-    iconAnchor: [30, 60], // Punto del ícono que se posicionará en las coordenadas
-    popupAnchor: [0, -60] // Punto desde donde se abrirá el popup
+    iconUrl: '/static/stores/images/custom-marker.png',
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [0, -60]
 });
 
 // Añadir marcadores de tiendas
@@ -20,9 +20,12 @@ var stores = JSON.parse(document.getElementById('store-data').textContent);
 console.log(stores);
 
 stores.forEach(function(store) {
-    var marker = L.marker([store.lat, store.lng], { icon: customIcon }).addTo(map); // Usar el ícono personalizado
+    var marker = L.marker([store.lat, store.lng], { icon: customIcon }).addTo(map);
     marker.bindPopup(
-        `<b>${store.name}</b><br>${store.description}<br><a href="${store.url}" class="btn btn-primary btn-sm mt-2">Ver productos</a>`
+        `<div style="color: white;">
+            <b>${store.name}</b><br>${store.description}<br>
+            <a href="${store.url}" class="btn btn-primary btn-sm mt-2">Ver productos</a>
+        </div>`
     );
     console.log(store);
 });
@@ -30,3 +33,11 @@ stores.forEach(function(store) {
 // Ajustar el mapa para mostrar todos los marcadores
 var bounds = L.latLngBounds(stores.map(store => [store.lat, store.lng]));
 map.fitBounds(bounds);
+
+// Añadir marcador por defecto con popup activo
+var defaultMarker = L.marker([12.1319899, -86.2698851]).addTo(map);
+defaultMarker.bindPopup(
+    `<div style="color: white;">
+        <b>Ubicación actual</b><br>Usted está aquí.
+    </div>`
+).openPopup(); // Abrir popup por defecto
